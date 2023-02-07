8ea9a5dad6069de83daf7babbefb8806354819b2
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-02-07 15:07:21 +0100

    Move vfr submodule to vstools

--- vsdeinterlace/__init__.py
+++ vsdeinterlace/__init__.py
@@ -4,4 +4,3 @@ from .funcs import *  # noqa: F401, F403
 from .helpers import *  # noqa: F401, F403
 from .ivtc import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
-from .vfr import *  # noqa: F401, F403
