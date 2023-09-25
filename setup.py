ae5ed89c0eed86a649b66ea4581d4acc08a7df8e
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2023-09-25 19:30:15 +0200

    Wobbly: Create base submodule

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setuptools.setup(
     install_requires=requirements,
     python_requires='>=3.10',
     packages=[
-        package_name
+        package_name,
+        f"{package_name}.wobbly"
     ],
     package_data={
         package_name: ['py.typed']
---
+++ vsdeinterlace/wobbly/__init__.py
@@ -0,0 +1,2 @@
+from .wobbly import *  # noqa: F401, F403
+from .wibbly import *  # noqa: F401, F403
