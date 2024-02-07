efb9e7bfee79732e47fc38bd5c06d67dc31b9097
Oleg Oshmyan <chortos@inbox.lv>
2024-02-07 02:00:28 +0200

    fix_telecined_fades: fix regression to no-op
    Commit 67c08e1f061e0ee4cbd9b9fdec11126d00f645d5 attempted to make
    fix_telecined_fades work for BFF clips but actually made it do nothing
    at all, regardless of field order: t1 and t2 were meant to be listed
    in the opposite order in the two lines of the expression in order to
    select one field and then the other field), but the order was, in fact,
    identical, so the same field was selected twice and divided by itself.
    
    This could be fixed by swapping the second instance of t1 and t2,
    but instead, take the opportunity to simplify the expression further:
    the second line just wants the sum of both fields, so there's no need
    to switch based on BF@ once again and look for "the other" field;
    just take "both fields" explicitly.

--- vsdeinterlace/combing.py
+++ vsdeinterlace/combing.py
@@ -61,8 +61,8 @@ def fix_telecined_fades(
     )
 
     fix = norm_expr(
-        props_clip, 'Y 2 % BF! BF@ x.f{t1}Avg{i} x.f{t2}Avg{i} ? AVG! '
-        'AVG@ 0 = x x {color} - AVG@ BF@ x.f{t1}Avg{i} x.f{t2}Avg{i} ? + 2 / AVG@ / * ? {color} +',
+        props_clip, 'Y 2 % x.f{t1}Avg{i} x.f{t2}Avg{i} ? AVG! '
+        'AVG@ 0 = x x {color} - x.ftAvg{i} x.fbAvg{i} + 2 / AVG@ / * ? {color} +',
         planes, i=f.norm_planes, color=colors, force_akarin=func,
         t1='b' if tff.is_tff else 't', t2='t' if tff.is_tff else 'b'
     )
