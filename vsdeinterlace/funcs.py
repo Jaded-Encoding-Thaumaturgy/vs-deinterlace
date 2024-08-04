d7a3a898bc690c7d0607e511ec0c45cb9ec7cb5f
Setsugennoao <setsugen@setsugen.dev>
2024-08-04 16:54:02 +0200

    Rework pulldow_credits (#33)
    * Clean up pulldown_credits
    
    * More cleanup
    
    * Fix formatting
    
    * More changes
    
    * xoxo
    
    * Remove deprecated fix_telecined_fades
    
    * Remove clip
    
    * More cleanup
    
    * Rewrite again and rename to telop_resample
    
    * Rename funcs
    
    * Add back framerates
    
    * Fix

--- vsdeinterlace/funcs.py
+++ vsdeinterlace/funcs.py
@@ -1,183 +1,132 @@
 from __future__ import annotations
 
-import warnings
 from typing import Any
 
-from vstools import DependencyNotFoundError, FieldBased, FieldBasedT, InvalidFramerateError, check_variable, core, vs
+from stgpytools import CustomIntEnum
+from vsdenoise import MVTools
+from vstools import InvalidFramerateError, check_variable, core, vs
 
 __all__ = [
-    'pulldown_credits'
+    'telop_resample'
 ]
 
 
-def pulldown_credits(
-    clip: vs.VideoNode, frame_ref: int, tff: bool | FieldBasedT | None = None,
-    interlaced: bool = True, dec: bool | None = None,
-    bob_clip: vs.VideoNode | None = None, qtgmc_args: dict[str, Any] | None = None
-) -> vs.VideoNode:
-    """
-    Deinterlacing function for interlaced credits (60i/30p) on top of telecined video (24p).
+class telop_resample(CustomIntEnum):
+    TXT60i_on_24telecined = 0
+    TXT60i_on_24duped = 1
+    TXT30p_on_24telecined = 2
 
-    This is a combination of havsfunc's dec_txt60mc, ivtc_txt30mc, and ivtc_txt60mc functions.
-    The credits are interpolated and decimated to match the output clip.
+    def __call__(self, bobbed_clip: vs.VideoNode, pattern: int, **mv_args: Any) -> vs.VideoNode:
+        """
+        :param bobbed_clip:             Bobbed clip. Framerate must be 60000/1001.
+        :param pattern:                 First frame in the pattern.
+        :param mv_args:                 Arguments to pass on to MVTools, used for motion compensation.
 
-    The function assumes you're passing a telecined clip (that's native 24p).
-    If your clip is already fieldmatched, decimation will automatically be enabled unless set it to False.
-    Likewise, if your credits are 30p (as opposed to 60i), you should set `interlaced` to False.
+        :return:                        Decimated clip with text resampled down to 24p.
 
-    The recommended way to use this filter is to trim out the area with interlaced credits,
-    apply this function, and `vstools.insert_clip` the clip back into a properly IVTC'd clip.
-    Alternatively, use `muvsfunc.VFRSplice` to splice the clip back in if you're dealing with a VFR clip.
+        :raises InvalidFramerateError:  Bobbed clip does not have a framerate of 60000/1001 (59.94)
+        """
 
-    :param clip:                    Clip to process. Framerate must be 30000/1001.
-    :param frame_ref:               First frame in the pattern. Expected pattern is ABBCD,
-                                    except for when ``dec`` is enabled, in which case it's AABCD.
-    :param tff:                     Top-field-first. `False` sets it to Bottom-Field-First.
-    :param interlaced:              60i credits. Set to false for 30p credits.
-    :param dec:                     Decimate input clip as opposed to IVTC.
-                                    Automatically enabled if certain fieldmatching props are found.
-                                    Can be forcibly disabled by setting it to `False`.
-    :param bob_clip:                Custom bobbed clip. If `None`, uses a QTGMC clip.
-                                    Framerate must be 60000/1001.
-    :param qtgmc_args:              Arguments to pass on to QTGMC.
-                                    Accepts any parameter except for FPSDivisor and TFF.
+        assert check_variable(bobbed_clip, telop_resample)
 
-    :return:                        IVTC'd/decimated clip with credits pulled down to 24p.
+        InvalidFramerateError.check(telop_resample, bobbed_clip, (60000, 1001))
 
-    :raises ModuleNotFoundError:    Dependencies are missing.
-    :raises ValueError:             Clip does not have a framerate of 30000/1001 (29.97).
-    :raises TopFieldFirstError:     No automatic ``tff`` can be determined.
-    :raises InvalidFramerateError:  Bobbed clip does not have a framerate of 60000/1001 (59.94)
-    """
+        invpos = (5 - pattern * 2 % 5) % 5
 
-    try:
-        from havsfunc import QTGMC  # type: ignore[import]
-    except ModuleNotFoundError:
-        raise DependencyNotFoundError(pulldown_credits, 'havsfunc')
+        offset = [0, 0, -1, 1, 1][pattern]
+        pattern = [0, 1, 0, 0, 1][pattern]
+        direction = [-1, -1, 1, 1, 1][pattern]
 
-    try:
-        from vsdenoise import prefilter_to_full_range
-    except ModuleNotFoundError:
-        from havsfunc import DitherLumaRebuild as prefilter_to_full_range  # type: ignore
-        warnings.warn("pulldown_credits: missing dependency `vsdenoise`!", ImportWarning)
+        ivtc_fps, ivtc_fps_div = (dict(fpsnum=x, fpsden=1001) for x in (24000, 12000))
 
-    assert check_variable(clip, "pulldown_credits")
+        pos = []
+        assumefps = 0
 
-    InvalidFramerateError.check(pulldown_credits, clip, (30000, 1001))
+        interlaced = self in (telop_resample.TXT60i_on_24telecined, telop_resample.TXT60i_on_24duped)
+        decimate = self is telop_resample.TXT60i_on_24duped
 
-    tff = FieldBased.from_param_or_video(tff, clip, True, pulldown_credits)
-    clip = FieldBased.ensure_presence(clip, tff)
+        cycle = 10 // (1 + interlaced)
 
-    qtgmc_kwargs = dict[str, Any](
-        SourceMatch=3, Lossless=2, TR0=2, TR1=2, TR2=3, Preset="Placebo"
-    ) | (qtgmc_args or {}) | dict(FPSDivisor=1, TFF=tff.field)
+        def bb(idx: int, cut: bool = False) -> vs.VideoNode:
+            if cut:
+                return bobbed_clip[cycle:].std.SelectEvery(cycle, [idx])
+            return bobbed_clip.std.SelectEvery(cycle, [idx])
 
-    if dec is not False:  # Automatically enable dec unless set to False
-        dec = any(x in clip.get_frame(0).props for x in {"VFMMatch", "TFMMatch"})
+        def intl(clips: list[vs.VideoNode], toreverse: bool, halv: list[int]) -> vs.VideoNode:
+            clips = [c[::2] if i in halv else c for i, c in enumerate(clips)]
+            if not toreverse:
+                clips = list(reversed(clips))
+            return core.std.Interleave(clips)
 
-        if dec:
-            warnings.warn("pulldown_credits: 'Fieldmatched clip passed to function! "
-                          "dec is set to `True`. If you want to disable this, set `dec=False`!'")
+        if interlaced:
+            if decimate:
+                cleanpos = 4
+                pos = [1, 2]
 
-    # motion vector and other values
-    field_ref = frame_ref * 2
-    frame_ref %= 5
-    invpos = (5 - field_ref) % 5
+                if invpos > 2:
+                    pos = [6, 7]
+                    assumefps = 2
+                elif invpos > 1:
+                    pos = [2, 6]
+                    assumefps = 1
+            else:
+                cleanpos = 1
+                pos = [3, 4]
 
-    offset = [0, 0, -1, 1, 1][frame_ref]
-    pattern = [0, 1, 0, 0, 1][frame_ref]
-    direction = [-1, -1, 1, 1, 1][frame_ref]
+                if invpos > 1:
+                    cleanpos = 6
+                    assumefps = 1
 
-    blksize = 16 if clip.width > 1024 or clip.height > 576 else 8
-    overlap = blksize // 2
+                if invpos > 3:
+                    pos = [4, 8]
+                    assumefps = 1
 
-    ivtc_fps = dict(fpsnum=24000, fpsden=1001)
-    ivtc_fps_div = dict(fpsnum=12000, fpsden=1001)
+            clean = bobbed_clip.std.SelectEvery(cycle, [cleanpos - invpos])
+            jitter = bobbed_clip.std.SelectEvery(cycle, [p - invpos for p in pos])
 
-    # Bobbed clip
-    bobbed = bob_clip or QTGMC(clip, **qtgmc_kwargs)
-
-    InvalidFramerateError.check(pulldown_credits, bobbed, (60000, 1001))
-
-    if interlaced:  # 60i credits. Start of ABBCD
-        if dec:  # Decimate the clip instead of properly IVTC
-            clean = bobbed.std.SelectEvery(5, [4 - invpos])
-
-            if invpos > 2:
+            if assumefps:
                 jitter = core.std.AssumeFPS(
-                    bobbed[0] * 2 + bobbed.std.SelectEvery(5, [6 - invpos, 7 - invpos]),
-                    **ivtc_fps)  # type:ignore[arg-type]
-            elif invpos > 1:
-                jitter = core.std.AssumeFPS(
-                    bobbed[0] + bobbed.std.SelectEvery(5, [2 - invpos, 6 - invpos]),
-                    **ivtc_fps)  # type:ignore[arg-type]
-            else:
-                jitter = bobbed.std.SelectEvery(5, [1 - invpos, 2 - invpos])
-        else:  # Properly IVTC
-            if invpos > 1:
-                clean = core.std.AssumeFPS(bobbed[0] + bobbed.std.SelectEvery(5, [6 - invpos]),
-                                           **ivtc_fps_div)  # type:ignore[arg-type]
-            else:
-                clean = bobbed.std.SelectEvery(5, [1 - invpos])
+                    bobbed_clip[0] * assumefps + jitter, **(ivtc_fps_div if cleanpos == 6 else ivtc_fps)
+                )
 
-            if invpos > 3:
-                jitter = core.std.AssumeFPS(bobbed[0] + bobbed.std.SelectEvery(5, [4 - invpos, 8 - invpos]),
-                                            **ivtc_fps)  # type:ignore[arg-type]
-            else:
-                jitter = bobbed.std.SelectEvery(5, [3 - invpos, 4 - invpos])
+            comp = MVTools(jitter, **mv_args).flow_interpolate()
 
-        jsup_pre = prefilter_to_full_range(jitter, 1.0).mv.Super(pel=2)
-        jsup = jitter.mv.Super(pel=2, levels=1)
-        vect_f = jsup_pre.mv.Analyse(blksize=blksize, isb=False, delta=1, overlap=overlap)
-        vect_b = jsup_pre.mv.Analyse(blksize=blksize, isb=True, delta=1, overlap=overlap)
-        comp = core.mv.FlowInter(jitter, jsup, vect_b, vect_f)
-        out = core.std.Interleave([comp[::2], clean] if dec else [clean, comp[::2]])
-        offs = 3 if dec else 2
-        return out[invpos // offs:]
-    else:  # 30i credits
+            out = intl([comp, clean], decimate, [0])
+            offs = 3 if decimate else 2
+
+            return out[invpos // offs:]
+
         if pattern == 0:
+            c1pos = [0, 2, 7, 5]
+            c2pos = [3, 4, 9, 8]
+
             if offset == -1:
-                c1 = core.std.AssumeFPS(bobbed[0] + bobbed.std.SelectEvery(
-                    10, [2 + offset, 7 + offset, 5 + offset, 10 + offset]), **ivtc_fps)  # type:ignore[arg-type]
-            else:
-                c1 = bobbed.std.SelectEvery(10, [offset, 2 + offset, 7 + offset, 5 + offset])
+                c1pos = [2, 7, 5, 10]
 
             if offset == 1:
-                c2 = core.std.Interleave([
-                    bobbed.std.SelectEvery(10, [4]),
-                    bobbed.std.SelectEvery(10, [5]),
-                    bobbed[10:].std.SelectEvery(10, [0]),
-                    bobbed.std.SelectEvery(10, [9])
-                ])
-            else:
-                c2 = bobbed.std.SelectEvery(10, [3 + offset, 4 + offset, 9 + offset, 8 + offset])
+                c2pos = []
+                c2 = core.std.Interleave([bb(4), bb(5), bb(0, True), bb(9)])
         else:
+            c1pos = [2, 4, 9, 7]
+            c2pos = [0, 1, 6, 5]
+
             if offset == 1:
-                c1 = core.std.Interleave([
-                    bobbed.std.SelectEvery(10, [3]),
-                    bobbed.std.SelectEvery(10, [5]),
-                    bobbed[10:].std.SelectEvery(10, [0]),
-                    bobbed.std.SelectEvery(10, [8])
-                ])
-            else:
-                c1 = bobbed.std.SelectEvery(10, [2 + offset, 4 + offset, 9 + offset, 7 + offset])
+                c1pos = []
+                c1 = core.std.Interleave([bb(3), bb(5), bb(0, True), bb(8)])
 
             if offset == -1:
-                c2 = core.std.AssumeFPS(bobbed[0] + bobbed.std.SelectEvery(
-                    10, [1 + offset, 6 + offset, 5 + offset, 10 + offset]), **ivtc_fps)  # type:ignore[arg-type]
-            else:
-                c2 = bobbed.std.SelectEvery(10, [offset, 1 + offset, 6 + offset, 5 + offset])
+                c2pos = [1, 6, 5, 10]
 
-        super1_pre = prefilter_to_full_range(c1, 1.0).mv.Super(pel=2)
-        super1 = c1.mv.Super(pel=2, levels=1)
-        vect_f1 = super1_pre.mv.Analyse(blksize=blksize, isb=False, delta=1, overlap=overlap)
-        vect_b1 = super1_pre.mv.Analyse(blksize=blksize, isb=True, delta=1, overlap=overlap)
-        fix1 = c1.mv.FlowInter(super1, vect_b1, vect_f1, time=50 + direction * 25).std.SelectEvery(4, [0, 2])
+        if c1pos:
+            c1 = bobbed_clip.std.SelectEvery(cycle, [c + offset for c in c1pos])
 
-        super2_pre = prefilter_to_full_range(c2, 1.0).mv.Super(pel=2)
-        super2 = c2.mv.Super(pel=2, levels=1)
-        vect_f2 = super2_pre.mv.Analyse(blksize=blksize, isb=False, delta=1, overlap=overlap)
-        vect_b2 = super2_pre.mv.Analyse(blksize=blksize, isb=True, delta=1, overlap=overlap)
-        fix2 = c2.mv.FlowInter(super2, vect_b2, vect_f2).std.SelectEvery(4, [0, 2])
+        if c2pos:
+            c2 = bobbed_clip.std.SelectEvery(cycle, [c + offset for c in c2pos])
 
-        return core.std.Interleave([fix1, fix2] if pattern == 0 else [fix2, fix1])
+        if offset == -1:
+            c1, c2 = (core.std.AssumeFPS(bobbed_clip[0] + c, **ivtc_fps) for c in (c1, c2))
+
+        fix1 = MVTools(c1, **mv_args).flow_interpolate(time=50 + direction * 25)
+        fix2 = MVTools(c2, **mv_args).flow_interpolate()
+
+        return intl([fix1, fix2], pattern == 0, [0, 1])
