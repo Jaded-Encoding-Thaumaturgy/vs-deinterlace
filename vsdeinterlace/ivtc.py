d0a41beacbc0dea417526ec330098911eb1fcef7
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2024-09-10 17:22:17 -0400

    Add VIVTC wrappers

--- vsdeinterlace/ivtc.py
+++ vsdeinterlace/ivtc.py
@@ -2,13 +2,17 @@ from __future__ import annotations
 
 from typing import Any
 
-from vstools import CustomEnum, FieldBased, FieldBasedT, InvalidFramerateError, VSFunctionKwArgs, core, join, vs
+from vstools import (
+    CustomEnum, FieldBased, FieldBasedT, InvalidFramerateError,
+    VSFunctionKwArgs, core, join, vs, find_prop_rfs, FunctionUtil
+)
 
 from .blending import deblend
 
 __all__ = [
     'IVTCycles',
     'sivtc', 'jivtc',
+    'vfm', 'vdecimate'
 ]
 
 
@@ -90,3 +94,71 @@ def jivtc(
     final = join(ivtced, final) if chroma_only else final
 
     return FieldBased.ensure_presence(final, FieldBased.PROGRESSIVE)
+
+
+def vfm(
+    clip: vs.VideoNode, tff: bool | None = None, field: int = 2, mode: int = 1, mchroma: bool = True,
+    cthresh: int = 9, mi: int = 80, chroma: bool = True, block: tuple | int = 16, y: tuple | bool = 16,
+    scthresh: float | int = 12, micmatch: int = 1, micout: bool = False, postprocess: vs.VideoNode | None = None
+) -> vs.VideoNode:
+    
+    func = FunctionUtil(clip, vfm, None, vs.YUV, 8)
+
+    if isinstance(block, int):
+        block = (block, block)
+    if isinstance(y, int):
+        y = (y, y)
+
+    if func.work_clip != clip:
+        clip2 = clip
+    else:
+        clip2 = None
+
+    fieldmatch = func.work_clip.vivtc.VFM(
+        order=tff, field=field, mode=mode, mchroma=mchroma, cthresh=cthresh, mi=mi, chroma=chroma, blockx=block[0], blocky=block[1],
+        y0=y[0], y1=y[1], scthresh=scthresh, micmatch=micmatch, micout=micout, clip2=clip2
+    )
+
+    if not postprocess:
+        fieldmatch = find_prop_rfs(fieldmatch, postprocess, prop="_Combed")
+    
+    return fieldmatch
+
+
+def vdecimate(
+    clip: vs.VideoNode, cycle: int = 5, chroma: bool = True, dupthresh: float | int = 1.1, scthresh: float | int = 15,
+    block: tuple | int = 32, ovr: str | None = None, dryrun: bool = False, weight: float = 0.0
+) -> vs.VideoNode:
+    
+    func = FunctionUtil(clip, vdecimate, None, vs.YUV, (8, 16))
+
+    if isinstance(block, int):
+        block = (block, block)
+
+    if dryrun:
+        weight = 0.0
+    if weight:
+        dryrun = True
+
+    if func.work_clip != clip:
+        clip2 = clip
+    else:
+        clip2 = None
+
+    decimate_args = dict(
+        cycle=cycle, chroma=chroma, dupthresh=dupthresh, scthresh=scthresh,
+        blockx=block[0], blocky=block[1], ovr=ovr, dryrun=dryrun, clip2=clip2
+    )
+
+    if dryrun:
+        stats = func.work_clip.vivtc.VDecimate(**decimate_args)
+        if not weight:
+            return stats
+        else:
+            decimate_args['dryrun'] = False
+            avg = clip.std.AverageFrames(weights=[0, 1 - weight, weight])
+            func.work_clip = find_prop_rfs(clip, avg, ref=stats, prop="VDecimateDrop")
+
+    decimate = func.work_clip.vivtc.VDecimate(**decimate_args)
+
+    return func.return_clip(decimate)
